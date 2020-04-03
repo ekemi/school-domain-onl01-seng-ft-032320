f0ae3c1db49f46374adc064d4bc95170539334e4
edmundo papi <edmundopierre@yahoo.com>
2020-04-03 12:34:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,8 +12,8 @@ class School
 
   def add_student(student_name, grade)
     if !@roster.has_key?(grade)
-         @roster[grade] = []
-       end
+        @roster[grade] = []
+    end
         @roster[grade] << student_name
  end
 
